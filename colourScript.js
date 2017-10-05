ec6a23f393d453ea387dfcef1c6dcf68fd32107f
Mike Jones <mikethespike77@gmail.com>
2017-10-05 20:34:48 +0700

    init all files

---
+++ colourScript.js
@@ -0,0 +1,26 @@
+var box = '<div class="resizable" class="ui-widget-content"> <h3 class="ui-widget-header">Resizable</h3> </div>'
+
+
+
+$(document).ready(function() {
+
+console.log('Ready mofo')
+
+	
+
+
+$("#box").on("click", function(){
+	console.log('box clicked');
+
+	$('#containment-wrapper').append(box)
+
+	console.log($(".resizable"));
+
+	$( function() {
+    $( ".resizable" ).resizable();
+    $( ".resizable").draggable({ containment: "#containment-wrapper", scroll: false });
+  } );
+
+});
+
+});
